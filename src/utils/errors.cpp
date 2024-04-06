9f7cd84d51ada5e908e8105051b2f59b220379f7
Souhaib LAANANI <souhaib.laanani@oracle.com>
2024-04-06 22:33:38 +0000

    Fix std c++98 incompatible functions (#2)
    * Fix std c++98 incompatible functions
    
    * Add missing include <cstdlib>

--- src/utils/errors.cpp
+++ src/utils/errors.cpp
@@ -6,7 +6,7 @@
 /*   By: slaanani <slaanani@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 20:59:12 by slaanani          #+#    #+#             */
-/*   Updated: 2024/04/06 21:46:06 by slaanani         ###   ########.fr       */
+/*   Updated: 2024/04/06 22:24:04 by slaanani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 void printErrorAndExit(int errorCode)
 {
-	std::string errorMessage = getErrorString(errorCode);
+	std::string errorMessage;
+	
+	errorMessage = getErrorString(errorCode);
 	std::cerr << "Error: Code " << errorCode << " - " << errorMessage << std::endl;
 	std::exit(errorCode);
 }
 
 std::string getErrorString(int errorCode)
 {
+	std::stringstream ss;
+	std::string errorCodeString;
 	std::map<int, std::string> errorMap;
-	errorMap.insert(std::make_pair(INVALID_ARGUMENT_ERROR, "Invalid argument provided"));
 
+	ss << errorCode;
+	errorCodeString = ss.str();
+	errorMap.insert(std::make_pair(INVALID_ARGUMENT_ERROR, "Invalid argument provided"));
 	if (errorMap.count(errorCode) > 0)
 		return errorMap[errorCode];
 	else
-		return "Unknown Error (Code: " + std::to_string(errorCode) + ")";
+		return "Unknown Error (Code: " + errorCodeString + ")";
 }
