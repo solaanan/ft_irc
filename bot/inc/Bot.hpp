12f87a8af74b4eb961035e43ce7002fc5dab5427
Souhaib LAANANI <sohaib.laanani@gmail.com>
2024-04-10 01:43:58 +0000

    Add bot

--- bot/inc/Bot.hpp
+++ bot/inc/Bot.hpp
@@ -13,5 +13,40 @@
 #ifndef BOT_HPP
 #define BOT_HPP
 
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <vector>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <ctime>
+
+class Bot {
+	private:
+		int sockfd_;
+		const char* server_;
+		int port_;
+		const char* password_;
+		const char* username_;
+		const char* nickname_;
+		int create_socket(const char* hostname, int port);
+		void close_socket();
+
+	public:
+		Bot(const char* server, int port, const char* password,
+					const char* username, const char* nickname) :
+			server_(server), port_(port), password_(password),
+			username_(username), nickname_(nickname) {}
+		bool connect_bot();
+		void send_data(const char* message);
+		std::string receive_data();
+		void handle_message(const std::string& message);
+		void roll_dice(const std::string& args);
+		void coinflip();
+		void time(); // Display current server time
+};
+
 
 #endif // BOT_HPP
--- bot/src/Bot.cpp
+++ bot/src/Bot.cpp
@@ -10,7 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-class Bot
-{
+#include "Bot.hpp"
 
-};
+int Bot::create_socket(const char *hostname, int port)
+{
+    struct sockaddr_in serverAddr;
+    struct hostent *server;
+
+    server = gethostbyname(hostname);
+    if (server == nullptr)
+    {
+        return -1;
+    }
+
+    sockfd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd_ == -1)
+    {
+        return -1;
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    memcpy(&serverAddr.sin_addr.s_addr, server->h_addr_list[0], server->h_length);
+    serverAddr.sin_port = htons(port);
+
+    if (connect(sockfd_, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        close(sockfd_);
+        return -1;
+    }
+
+    return sockfd_;
+}
+
+void Bot::close_socket()
+{
+    close(sockfd_);
+}
